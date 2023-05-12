e5be4dea5567564433a9434e1774982d9051d9ac
Leandro Chaves <leandropadilhachaves@gmail.com>
2023-05-12 16:14:07 -0300

    Indo para a reta final

---
+++ desafioFinal/desafioFinal.js
@@ -0,0 +1,18 @@
+function btnDesafioFinal(){
+let opcoes = prompt(`
+Digite 1, 2 ou 3 para escolher a opção correta:
+1: Encontrar os vilões e detê-los antes de soltar a bomba
+2: Criar uma super proteção para as bombas não atingirem o planeta.
+3: Criar bombas para contra atacarem e destruírem a dos bandidos.`)
+
+if (opcoes == 1){
+    alert ("Você venceu. Deteve os vilões e salvou o planeta.");
+    location.replace("../vencedor/vencedor.html");
+    }else if (opcoes == 2){
+          alert ("Você perdeu. Sua proteção falhou e o planeta foi destruído.");
+          location.replace("../gameOver/gameOver.html")
+          }  else if (opcoes == 3){
+                  alert ("Você peerdeu, as bombas se chocaram uma com as outras e o planeta foi destruído.");
+                  location.replace("../gameOver/gameOver.html");
+          }
+}
